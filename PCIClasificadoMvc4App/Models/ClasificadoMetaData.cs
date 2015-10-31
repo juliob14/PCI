5e7077928fbc00033d6714f906e068185634fd05
juliob14 <assassinpoint@gmail.com>
2015-10-31 13:44:31 -0700

    otro commit

---
+++ PCIClasificadoMvc4App/Models/ClasificadoMetaData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PCIClasificadoMvc4App.Models
+{
+    public class ClasificadoMetaData
+    {
+        [Display(Name = "Mensaje")]
+        public String clasificadoTexto;
+        [Display(Name = "Desde")]
+        public String publicacionFI;
+        [Display(Name = "Hasta")]
+        public String publicacionFF;
+
+    }
+    [MetadataType(typeof(ClasificadoMetaData))]
+    public partial class Clasificado 
+    {
+    }
+}
